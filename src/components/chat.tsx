6453495e24c1d0574bf104c773bd76be5a8289d4
Luka <luka.sesar99@gmail.com>
2023-05-16 12:18:48 +0200

    Feat: added time sent to message and rendered new message before fetched with exclamation mark, separated code to multiple components

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,149 +1,11 @@
 "use client";
 
-import { FC, useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import "./chat.css";
+import StartingScreen from "./starting-screen";
+import ChatScreen, { Message } from "./chat-screen";
 
-type Message = {
-  id: string;
-  userId: string;
-  content: string;
-  username: string;
-};
-
-type StartingScreenProps = {
-  confirmUsername: (username: string) => void;
-};
-
-const StartingScreen: FC<StartingScreenProps> = ({ confirmUsername }) => {
-  const [username, setUsername] = useState<string>("");
-
-  return (
-    <div className="w-80 h-52">
-      <form className="flex justify-center items-center gap-4 flex-col p-6 bg-gray-200 dark:bg-slate-700 w-full h-full rounded-xl shadow-2xl">
-        <input
-          className="py-2 px-4 focus:outline-none text-black rounded-md"
-          type="text"
-          id="username"
-          placeholder="Enter your username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <button
-          className="p-2 rounded-lg transition-all bg-cyan-600 hover:bg-cyan-500 px-4 py-2 text-slate-50 font-bold"
-          onClick={(e) => {
-            confirmUsername(username);
-            e.preventDefault();
-          }}
-          type="submit"
-        >
-          Start Chatting
-        </button>
-      </form>
-    </div>
-  );
-};
-
-type ChatScreenProps = {
-  messages: Message[];
-  userId: string;
-  username: string;
-};
-
-const MyMessageDisplay = ({ content }: { content: string }) => {
-  return (
-    <div className="flex justify-end">
-      <div className="text-white w-fit bg-emerald-500 min-w-[33%] py-3 px-4 rounded-2xl max-w-[70%] rounded-tr-none break-words">
-        {content}
-      </div>
-    </div>
-  );
-};
-
-const MessageDisplay = ({
-  content,
-  username,
-}: {
-  content: string;
-  username: string;
-}) => {
-  return (
-    <div className="flex flex-col">
-      <strong className="text-cyan-600 break-words">{username}:</strong>
-      <div className="text-white w-fit bg-cyan-500 font-medium min-w-[33%] py-3 px-4 max-w-[70%] rounded-2xl rounded-tl-none break-words">
-        {content}
-      </div>
-    </div>
-  );
-};
-
-const ChatScreen: FC<ChatScreenProps> = ({ userId, username, messages }) => {
-  const [newMessage, setNewMessage] = useState<string>("");
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
-
-  return (
-    <div className="flex justify-center items-center gap-4 flex-col max-w-[1100px] bg-gray-200 dark:bg-slate-700 w-full h-full sm:rounded-md sm:w-4/5 sm:h-4/5 shadow-2xl">
-      <div className="flex flex-col gap-3 flex-1 p-3 overflow-y-auto no-scroll w-full">
-        {messages.map((message) => {
-          if (message.userId === userId) {
-            return (
-              <MyMessageDisplay key={message.id} content={message.content} />
-            );
-          }
-          return (
-            <MessageDisplay
-              key={message.id}
-              content={message.content}
-              username={message.username}
-            />
-          );
-        })}
-        <div ref={messagesEndRef} style={{ overflowAnchor: "auto", height: "1px" }}></div>
-      </div>
-      <form className="flex w-full p-3 bg-gray-300 sm:rounded-b-md dark:bg-slate-600">
-        <input
-          className="flex-1 focus:outline-none min-w-[150px] py-2 px-4 text-black rounded-md rounded-r-none"
-          type="text"
-          id="newMessage"
-          placeholder="New message"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
-        <button
-          className="cursor-pointer rounded-l-none p-2 rounded-lg transition-all bg-cyan-600 hover:bg-cyan-500 px-4 py-2 text-slate-50 font-bold"
-          disabled={!newMessage}
-          onClick={(e) => {
-            e.preventDefault();
-            fetch("/api/chat", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                id: nanoid(),
-                username,
-                userId,
-                content: newMessage,
-              }),
-            });
-            setNewMessage("");
-          }}
-          type="submit"
-        >
-          Send
-        </button>
-      </form>
-    </div>
-  );
-};
 
 const Chat = () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -161,13 +23,16 @@ const Chat = () => {
 
   useEffect(() => {
     const eventSource = new EventSource("/api/chat");
+    
     eventSource.addEventListener("chat.message", (event) => {
       const message = JSON.parse(event.data) as Message;
-      setMessages((messages) => [...messages, message]);
+      setMessages((messages) => [...messages.filter(e => e.id !== message.id), message]);
     });
+
     setUsername(localStorage.getItem("username") || "");
     setUserId(localStorage.getItem("userId") || null);
     setLoading(false);
+
     return () => {
       eventSource.close();
     };
